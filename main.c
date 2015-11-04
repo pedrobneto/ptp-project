e843dff1bc5b03c9fb932d238289a1595f516fbd
pedrobneto <pedro_paivaneto@hotmail.com>
2015-11-04 14:53:28 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main()
+
+{
+
+    char file[50];
+    printf("Digite o nome do arquivo sem extensao.");
+    scanf("%s", file);
+
+    FILE * img;
+
+    img = fopen ("file.ppm", "r");
+
     return 0;
+
 }
+
